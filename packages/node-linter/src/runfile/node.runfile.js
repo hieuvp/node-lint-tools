6c8f8a039f7a276e0634db374fc9e678fde391bb
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-03-19 00:28:00 +0700

    feat(eslint): using eslint

--- packages/node-linter/src/runfile/node.runfile.js
+++ packages/node-linter/src/runfile/node.runfile.js
@@ -1,5 +1,17 @@
 const { options, run } = require('runjs');
+const isEmpty = require('lodash.isempty');
 
 module.exports = function lint(...args) {
+  if (isEmpty(args)) {
+    throw new TypeError('Please specify patterns or files');
+  }
+
   const { fix } = options(this);
+
+  // ESLint - The pluggable linting utility for JavaScript and JSX
+  const eslint = `eslint ${fix === true ? '--fix ' : ''}${args.join(' ')}`;
+  run(eslint);
+
+  // JSON Lint - A JSON parser and validator
+  const jsonlint = 'jsonlint';
 };
