2e8918abbf4a8969a96567575a90db30819b5e43
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-03-27 20:35:06 +0700

    docs(runner): add docs why using function keyword

--- packages/node-linter/src/runfile/node.runfile.js
+++ packages/node-linter/src/runfile/node.runfile.js
@@ -4,6 +4,7 @@ const eslint = require('./eslint.runfile');
 const jsonlint = require('./jsonlint.runfile');
 
 module.exports = function lint(...args) {
+  // If using Arrow Function here, the value of "this" will be lost
   const { fix } = options(this);
 
   // ESLint - The pluggable linting utility for JavaScript and JSX
