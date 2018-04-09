a928fc1953c64466843e92b9f47029a9db923d24
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-09 12:04:35 +0700

    feat(linters-args-validation): input validation when running linters cli

--- packages/node-linter/src/runfile/node.runfile.js
+++ packages/node-linter/src/runfile/node.runfile.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const minimatch = require('minimatch');
 const { options } = require('runjs');
 
+const blacklist = require('./blacklist.runfile');
 const { eslint } = require('./eslint.runfile');
 const { jsonlint } = require('./jsonlint.runfile');
 const { prettier } = require('./prettier.runfile');
@@ -9,24 +12,41 @@ const { prettier } = require('./prettier.runfile');
  */
 module.exports = function lint(...args) {
   if (args.length === 0) {
-    throw new TypeError('Please specify patterns or files');
+    throw new Error('Please specify patterns or files');
   }
 
   if (args.includes('.') && args.length !== 1) {
-    throw new TypeError(
+    throw new Error(
       "Wildcard (.) is already covered everything, there's no need to define more arguments"
     );
   }
 
-  // If using Arrow Function here, the value of "this" will be lost
+  // Add trailing slash to directory
+  const paths = args.map(path => {
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    const stats = fs.statSync(path);
+    if (path !== '.' && stats.isDirectory()) {
+      return path.replace(/\/*$/, '/');
+    }
+    return path;
+  });
+
+  // Forbid path that is in the blacklist
+  paths.forEach(path => {
+    if (blacklist.some(pattern => minimatch(path, pattern))) {
+      throw new Error(`Path "${path}" is blacklisted`);
+    }
+  });
+
+  // If using Arrow Function, the value of "this" will be lost
   const { ci, fix } = options(this);
 
   // ESLint - The pluggable linting utility for JavaScript and JSX
-  eslint(args, { ci: !!ci, fix: !!fix });
+  eslint(paths, { ci: !!ci, fix: !!fix });
 
   // JSON Lint - A JSON parser and validator
-  jsonlint(args, { ci: !!ci, fix: !!fix });
+  jsonlint(paths, { ci: !!ci, fix: !!fix });
 
   // Prettier - An opinionated code formatter
-  prettier(args, { ci: !!ci, fix: !!fix });
+  prettier(paths, { ci: !!ci, fix: !!fix });
 };
