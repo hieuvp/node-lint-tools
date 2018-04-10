0cd60700e3698982fae1febe453a8d5f255bb9f4
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-10 11:53:20 +0700

    test(linters-args-validation): update snapshots

--- packages/node-linter/src/runfile/node.runfile.test.js
+++ packages/node-linter/src/runfile/node.runfile.test.js
@@ -45,13 +45,13 @@ describe('args validation', () => {
   });
 
   ['src', 'test/', 'config.js'].forEach(args => {
-    it(`should throw an error because ${args} does not exist`, () => {
+    it(`should throw an error because "${args}" does not exist`, () => {
       expect(() => lint(args)).toThrowErrorMatchingSnapshot();
     });
   });
 
   ['package.json', 'node_modules', 'packages/node-linter/node_modules/'].forEach(args => {
-    it(`should throw an error because ${args} is in the blacklist`, () => {
+    it(`should throw an error because "${args}" is in the blacklist`, () => {
       expect(() => lint(args)).toThrowErrorMatchingSnapshot();
     });
   });
