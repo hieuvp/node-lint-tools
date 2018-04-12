8cf23e37332e49615248860e60ea2533d1b56c59
Hieu Van <hieuvanphu91@gmail.com>
2018-04-12 11:29:55 +0700

    test: linters args validation (#14)
    * test(linters invocation): test runner order
    
    * test(linters-invocation): test transformed args
    
    * chore(linters-args-validation): change unit tests description

--- packages/node-linter/src/runfile/node.runfile.test.js
+++ packages/node-linter/src/runfile/node.runfile.test.js
@@ -1,7 +1,11 @@
+const { options } = require('runjs');
+
 const lint = require('./node.runfile');
 const { eslint } = require('./eslint.runfile');
 const { jsonlint } = require('./jsonlint.runfile');
 const { prettier } = require('./prettier.runfile');
+
+jest.mock('runjs', () => ({ options: jest.fn() }));
 
 jest.mock('./eslint.runfile', () => ({ eslint: jest.fn() }));
 jest.mock('./jsonlint.runfile', () => ({ jsonlint: jest.fn() }));
@@ -26,6 +30,13 @@ const validatedArgs = [
  */
 
 describe('args validation', () => {
+  beforeEach(() => {
+    options.mockImplementation(() => ({}));
+    eslint.mockImplementation(() => Promise.resolve());
+    jsonlint.mockImplementation(() => Promise.resolve());
+    prettier.mockImplementation(() => Promise.resolve());
+  });
+
   afterEach(() => {
     jest.resetAllMocks();
   });
@@ -53,6 +64,63 @@ describe('args validation', () => {
   ['package.json', 'node_modules', 'packages/node-linter/node_modules/'].forEach(args => {
     it(`should throw an error because "${args}" is in the blacklist`, () => {
       expect(() => lint(args)).toThrowErrorMatchingSnapshot();
+    });
+  });
+});
+
+describe('linters invocation', () => {
+  // eslint-disable-next-line promise/avoid-new
+  const sleep = ms => () => new Promise(resolve => setTimeout(resolve, ms));
+
+  beforeEach(() => {
+    options.mockImplementation(() => ({}));
+    eslint.mockImplementation(() => Promise.resolve());
+    jsonlint.mockImplementation(() => Promise.resolve());
+    prettier.mockImplementation(() => Promise.resolve());
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  validatedArgs.forEach(args => {
+    it(`should run all linters in order when giving ${JSON.stringify(args)}`, async () => {
+      eslint.mockImplementation(sleep(2));
+      jsonlint.mockImplementation(sleep(2));
+      prettier.mockImplementation(sleep(2));
+
+      await lint(...args);
+
+      expect(eslint).toHaveBeenCalledTimes(1);
+      expect(eslint).toHaveBeenCalledBefore(jsonlint);
+      expect(eslint).toHaveBeenCalledBefore(prettier);
+
+      expect(jsonlint).toHaveBeenCalledTimes(1);
+      expect(jsonlint).toHaveBeenCalledBefore(prettier);
+
+      expect(prettier).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  [
+    { args: '.', transformedArgs: ['.'] },
+    { args: 'runfile.js', transformedArgs: ['runfile.js'] },
+    { args: 'jest.config.js', transformedArgs: ['jest.config.js'] },
+    { args: 'packages', transformedArgs: ['packages/'] },
+    { args: 'packages/', transformedArgs: ['packages/'] },
+    { args: 'packages///', transformedArgs: ['packages/'] }
+  ].forEach(({ args, transformedArgs }) => {
+    it(`should transform "${args}" to "${transformedArgs}" before delegating to linters`, async () => {
+      const opts = { ci: undefined, fix: true };
+      const transformedOpts = { ci: false, fix: true };
+
+      options.mockImplementation(() => opts);
+
+      await lint(args);
+
+      expect(eslint).toBeCalledWith(transformedArgs, transformedOpts);
+      expect(jsonlint).toBeCalledWith(transformedArgs, transformedOpts);
+      expect(prettier).toBeCalledWith(transformedArgs, transformedOpts);
     });
   });
 });
