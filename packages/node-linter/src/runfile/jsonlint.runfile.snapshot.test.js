43caf83221114d10439bd81ec806811fbf2e76f3
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-12 12:38:24 +0700

    test(jsonlint-parser): add snapshot tests

--- packages/node-linter/src/runfile/jsonlint.runfile.snapshot.test.js
+++ packages/node-linter/src/runfile/jsonlint.runfile.snapshot.test.js
@@ -10,7 +10,7 @@ const { parseJSONLintArgs } = require('./jsonlint.runfile');
   'packages/hapi-linter/package.json',
   'packages/node-linter/src/runfile/blacklist.runfile.json'
 ].forEach(args => {
-  it(`should match snapshot when parsing ${args}`, () => {
+  it(`should match snapshot when parsing "${args}"`, () => {
     expect(parseJSONLintArgs(args)).toMatchSnapshot();
   });
 });
