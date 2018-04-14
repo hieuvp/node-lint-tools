d16c3a406870236e6f93c1578d332445012da4ec
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-14 20:26:17 +0700

    feat(promise): add sleep unit tests

--- packages/node-linter/src/utils/promise.utils.js
+++ packages/node-linter/src/utils/promise.utils.js
@@ -2,8 +2,18 @@
  * @param {number} ms
  * @returns {Promise<any>}
  */
-// eslint-disable-next-line promise/avoid-new
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = ms => {
+  if (typeof ms !== 'number' || Number.isNaN(ms)) {
+    throw new TypeError(`Expected "Number", instead got "${ms}: ${typeof ms}"`);
+  }
+
+  if (ms <= 0) {
+    throw new Error('Millisecond must be a positive number');
+  }
+
+  // eslint-disable-next-line promise/avoid-new
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
 module.exports = {
   sleep
--- packages/node-linter/src/utils/promise.utils.test.js
+++ packages/node-linter/src/utils/promise.utils.test.js
@@ -1,1 +1,15 @@
-describe('sleep', () => {});
+const { sleep } = require('./promise.utils');
+
+describe('sleep', () => {
+  jest.useFakeTimers();
+
+  ['', () => {}, {}, [], NaN, false, undefined, null].forEach(args => {
+    it(`should throw an error because "${args}" is not a number`, () =>
+      expect(() => sleep(args)).toThrowErrorMatchingSnapshot());
+  });
+
+  it('should only sleep with a positive number', () =>
+    expect(() => sleep(0)).toThrowErrorMatchingSnapshot());
+
+  it('should sleep with a given time', () => expect(sleep(10)).resolves.toBe('lemon'));
+});
