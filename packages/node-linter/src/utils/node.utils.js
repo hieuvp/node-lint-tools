6435238e51ed16f9bf3bdc491b35ff0de3f617a9
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-18 18:20:38 +0700

    test(shell-utils): update tests

--- packages/node-linter/src/utils/node.utils.js
+++ packages/node-linter/src/utils/node.utils.js
@@ -1,9 +1,9 @@
 const glob = require('./glob.utils');
 const promise = require('./promise.utils');
-const run = require('./run.utils');
+const shell = require('./shell.utils');
 
 module.exports = {
   glob,
   promise,
-  run
+  shell
 };
--- packages/node-linter/src/utils/shell.utils.js
+++ packages/node-linter/src/utils/shell.utils.js
@@ -8,6 +8,9 @@ const { run } = require('runjs');
 const exec = (command, opts = {}) => {
   const async = true;
   const stdio = 'pipe';
+  // TODO: validate cmd input
+  // TODO: trim cmd input
+  // TODO: remove double spaces ...
 
   // TODO: opts to swallow error
   return run(command, {
