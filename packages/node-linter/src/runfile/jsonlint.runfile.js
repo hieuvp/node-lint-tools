9130b8a18872450cc0f98687ff0e024fa8ee9a96
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-18 17:50:29 +0700

    chore(jsonlint): tmp push

--- packages/node-linter/src/runfile/jsonlint.runfile.js
+++ packages/node-linter/src/runfile/jsonlint.runfile.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const glob = require('glob');
 const minimatch = require('minimatch');
+const { diffLines } = require('diff');
 const { run } = require('runjs');
 
 const {
@@ -48,8 +49,47 @@ const parseJSONLintArgs = args => {
  * @param {boolean} opts.fix
  * @returns {Promise}
  */
-const jsonlint = async (args, opts = {}) => {
+const jsonlint = (args, opts = {}) => {
   const { ci = false, fix = false } = opts;
+
+  let files = args.reduce(
+    (accumulator, path) => [...accumulator, ...parseJSONLintArgs(path)],
+    []
+  );
+  files = [...new Set(files)];
+
+  return files.reduce(
+    (accumulator, file) =>
+      accumulator
+        .then(() => {
+          console.log('Run ...');
+          return run(`jsonlint --sort-keys ${fix === true ? '--in-place' : ''} ${file}`, {
+            async: true,
+            stdio: 'pipe'
+          }).then(output => {
+            const fileContent = fs.readFileSync(file).toString();
+            console.log('fileContent =', fileContent);
+
+            const difference = diffLines(fileContent, output);
+            // difference.forEach((part) => {
+            //   const FgRed = '\x1b[31m';
+            //   const FgGreen = '\x1b[32m';
+            //   const color = part.added ? FgGreen : part.removed ? FgRed : 'grey';
+            //   console.log(color, `- or + ${part.value}`);
+            // });
+            console.log('difference =', difference);
+          });
+        })
+        .catch(error => {
+          console.log('Error Appear');
+          console.log('error =', error);
+
+          if (ci === true) {
+            throw error;
+          }
+        }),
+    Promise.resolve()
+  );
 };
 
 module.exports = {
