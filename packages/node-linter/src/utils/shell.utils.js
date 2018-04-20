8fc197359a27158d707dfe1840aebe882d34116b
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-20 15:38:00 +0700

    feat(shell-utils): exec

--- packages/node-linter/src/utils/shell.utils.js
+++ packages/node-linter/src/utils/shell.utils.js
@@ -1,21 +1,42 @@
+const dargs = require('dargs');
 const { run } = require('runjs');
 
 /**
  * @param {string} command
+ * @param {Object} args - minimist argument object
  * @param {Object} opts
+ * @param {boolean} opts.errorIgnored
  * @returns {Promise}
  */
-const exec = (command, opts = {}) => {
+const exec = (command, args = {}, opts = {}) => {
+  if (typeof command !== 'string') {
+    throw new TypeError(`Expected "String", instead got "${command}: ${typeof command}"`);
+  }
+
+  if (command.length === 0) {
+    throw new Error('Command must not be empty');
+  }
+
+  const { errorIgnored = false } = opts;
+
+  if (typeof errorIgnored !== 'boolean') {
+    throw new TypeError(
+      `Expected "Boolean", instead got "${errorIgnored}: ${typeof errorIgnored}"`
+    );
+  }
+
+  const builder = `${command.replace(/ {2,}/g, ' ')} ${dargs(args).join(' ')}`.trim();
   const async = true;
   const stdio = 'pipe';
-  // TODO: validate cmd input
-  // TODO: trim cmd input
-  // TODO: remove double spaces ...
-
-  // TODO: opts to swallow error
-  return run(command, {
+  return run(builder, {
     async,
     stdio
+  }).catch(error => {
+    if (errorIgnored) {
+      return error;
+    }
+
+    throw error;
   });
 };
 
