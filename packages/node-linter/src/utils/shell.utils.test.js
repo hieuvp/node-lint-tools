d40eddca91eb91c7c7793c9c29e88298adcdc49f
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-04-23 19:03:52 +0700

    test(shell-utils): update unit tests

--- packages/node-linter/src/utils/shell.utils.test.js
+++ packages/node-linter/src/utils/shell.utils.test.js
@@ -4,13 +4,15 @@ describe('exec', () => {
   describe('command', () => {
     [[], true, undefined, null, 1].forEach(command => {
       it(`should throw an error because "${JSON.stringify(command)}" is not a string`, () => {
-        expect(() => exec(command)).toThrowErrorMatchingSnapshot();
+        const fn = () => exec(command);
+        expect(fn).toThrowErrorMatchingSnapshot();
       });
     });
 
     ['', ' ', '     '].forEach(command => {
       it(`should throw an error because "${command}" is an empty string`, () => {
-        expect(() => exec(command)).toThrowErrorMatchingSnapshot();
+        const fn = () => exec(command);
+        expect(fn).toThrowErrorMatchingSnapshot();
       });
     });
   });
@@ -22,19 +24,21 @@ describe('exec', () => {
       [[], null, 1].forEach(errorIgnored => {
         it(`should throw an error because "${JSON.stringify(
           errorIgnored
-        )}" is not a boolean`, () =>
-          expect(() =>
-            exec(command, undefined, { errorIgnored })
-          ).toThrowErrorMatchingSnapshot());
+        )}" is not a boolean`, () => {
+          const fn = () => exec(command, undefined, { errorIgnored });
+          expect(fn).toThrowErrorMatchingSnapshot();
+        });
       });
 
-      it('should be able to reject an error by default', () =>
-        expect(exec(command)).rejects.toThrowErrorMatchingSnapshot());
+      it('should be able to reject an error by default', () => {
+        const promise = exec(command);
+        return expect(promise).rejects.toThrowErrorMatchingSnapshot();
+      });
 
-      it('should swallow the error when passing a true value', () =>
-        expect(exec(command, undefined, { errorIgnored: true })).resolves.toBeInstanceOf(
-          Error
-        ));
+      it('should swallow the error when passing a true value', () => {
+        const promise = exec(command, undefined, { errorIgnored: true });
+        return expect(promise).resolves.toBeInstanceOf(Error);
+      });
     });
   });
 });
