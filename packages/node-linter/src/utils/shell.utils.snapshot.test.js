ed6cbd10b2d72ed3fcdceee7b1192141508c4416
Hieu Van <hieuvanphu91@gmail.com>
2018-04-30 16:16:57 +0700

    feat(shell-utils): decorate (#25)
    * build(npm): update package-lock.json
    
    * test(decorate): add snapshot tests
    
    * feat(shell-utils): decorate
    
    * test(decorate): update unit tests
    
    * test(decorate): update unit tests

---
+++ packages/node-linter/src/utils/shell.utils.snapshot.test.js
@@ -0,0 +1,19 @@
+const { decorate } = require('./shell.utils');
+
+describe('decorate', () => {
+  [
+    'ls',
+    'ls -lia',
+    'which atom',
+    'brew cask outdated',
+    'docker info | grep -E "Containers|Images"',
+    '/usr/local/bin/npm -g outdated --parseable --depth=0',
+    'ls "/Users/hieuvp/Library/Preferences" | grep "^IntelliJIdea"',
+    'tree /Volumes/Data/Workspace -L 2 > "/Volumes/Data/Dropbox/Backup/Workspace.txt"',
+    'cd /Volumes/Data/Workspace/Farmers/logtan-api && git branch placeholder && git checkout placeholder'
+  ].forEach(command => {
+    it(`should match snapshot with command "${command}"`, () => {
+      expect(decorate(command)).toMatchSnapshot();
+    });
+  });
+});
